7a64e03f6a394b36218eec1b75416e0dcc33c4c0
Vladimir Novick <vladimirnovick@Vladimirs-MacBook-Pro.local>
2020-01-14 14:36:18 +0200

    THis is ok

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 import BaseButton from '@/components/shared/BaseButton';
+import store from './store'
 
 Vue.component('BaseButton', BaseButton)
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
